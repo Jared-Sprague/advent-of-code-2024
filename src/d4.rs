52e01ae3cbaa2abec350a83943e3dc58967a3da6
Jared Sprague <jared@caramelcode.com>
2024-12-04 01:09:05 -0500

    started day 4

--- src/d4.rs
+++ src/d4.rs
@@ -1,19 +1,70 @@
 //! A solution to day 4 year 2024.
 //! https://adventofcode.com/2024/day/4
 
-type Model = u8;
-type Answer = String;
+use rayon::vec;
+
+type Model = Puzzle;
+type Answer = u32;
+
+pub struct Puzzle {
+    input_matrix: Vec<Vec<char>>,
+    horizantal: String,
+    vertical: String,
+    diagnal: String,
+}
+
+impl Puzzle {
+    pub fn new(input_matrix: Vec<Vec<char>>) -> Puzzle {
+        let mut horizantal: String = String::new();
+        let mut vertical: String = String::new();
+        let mut diagnal: String = String::new();
+
+        // generate horizantal string
+        for row in &input_matrix {
+            horizantal.extend(row);
+        }
+
+        Puzzle {
+            input_matrix,
+            horizantal,
+            vertical,
+            diagnal,
+        }
+    }
+
+    pub fn find_horizantal(&self) -> u32 {
+        let mut total_horizantal: u32 = 0;
+
+        let re = Regex::new(r"XMAS|SAMX").unwrap(); // Create the regex pattern
+
+        // Find all matches
+        for mat in re.find_iter(text) {
+            println!("Found match: {}", mat.as_str());
+        }
+
+        total_horizantal
+    }
+}
 
 pub fn parse(input: String) -> Model {
-    0
+    let lines: Vec<&str> = input.trim().split("\n").collect();
+    let mut input_matrix: Vec<Vec<char>> = vec![];
+
+    for line in lines {
+        let chars: Vec<char> = line.chars().collect();
+        input_matrix.push(chars);
+    }
+
+    Model::new(input_matrix)
 }
 
 pub fn part1(model: Model) -> Answer {
-    "incomplete".to_string()
+    dbg!(model.horizantal);
+    0
 }
 
 pub fn part2(model: Model) -> Answer {
-    "incomplete".to_string()
+    0
 }
 
 // #[cfg(test)]
